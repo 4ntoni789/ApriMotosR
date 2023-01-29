c3c28e04667fbc5be84886a5c873f95a3786ce95
Antoni Parra <antonyparra789@gmail.com>
2023-01-29 06:13:39 -0500

    optimizando la version Respo

--- src/paginas/ModeloMoto.js
+++ src/paginas/ModeloMoto.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import "../estilos/modeloMotos.css";
 import themeContext, { moto } from "../context/themeContext.js";
-import MotoMostrar from '../componentes/MotoMostrar';
 import Footer from '../componentes/Footer';
 import BntDesCatalogo from '../componentes/BntDesCatalogo';
 import ReseñaMoto from '../componentes/ReseñaMoto';
@@ -10,19 +9,23 @@ import MotosMensaje from '../componentes/MotosMensaje';
 import ImgGrande from '../componentes/ImgGrande';
 import SliderMoto from "../componentes/SliderMoto.js"
 import EspMoto from '../componentes/EspMoto';
+import { Suspense, lazy } from 'react';
+const MotoMostrar = lazy(() => import("../componentes/MotoMostrar"));
 
 function ModeloMoto(props) {
 	const { name, id } = useParams();
-	let motoIden =[];
+	let motoIden = [];
 	return (
 		<themeContext.Provider value={moto}>
 			<div className='contModeloMoto contInicio2' >
-				{moto.map((m) => {
-					if (m.name == name) {
-						motoIden = m;
-						return (<MotoMostrar key={m.key} src={m.srcM1||m.src} title={m.name} precio={m.precio} referencia={m.ref} />)
-					}
-				})}
+				<Suspense fallback={<h1>cargando...</h1> /*<Loading/>*/}>
+					{moto.map((m) => {
+						if (m.name == name) {
+							motoIden = m;
+							return (<MotoMostrar key={m.key} src={m.srcM1 || m.src} title={m.name} precio={m.precio} referencia={m.ref} />)
+						}
+					})}
+				</Suspense>
 				<BntDesCatalogo />
 				<ReseñaMoto />
 				<MotosMensaje
@@ -47,10 +50,10 @@ function ModeloMoto(props) {
 					subMensaje="UNA NUEVA EXPERIENCIA DE CONDUCCIÓN"
 					parrafo="La Aprilia RS 660 utiliza la experiencia ganadora de Aprilia para establecer nuevos estándares en esta categoría de vehículo. La ciclística tope de gama maximiza la experiencia de conducción dinámica gracias al chasis y basculante de aluminio con características de construcción únicas, que garantizan una distribución óptima del peso y la máxima ligereza."
 				/>
-				<ImgGrande src={motoIden.srcM1} title={motoIden.name}/>
-				<EspMoto/>
+				<ImgGrande src={motoIden.srcM1} title={motoIden.name} />
+				<EspMoto />
 			</div>
-				<SliderMoto/>
+			<SliderMoto />
 			<Footer />
 		</themeContext.Provider>
 	);
